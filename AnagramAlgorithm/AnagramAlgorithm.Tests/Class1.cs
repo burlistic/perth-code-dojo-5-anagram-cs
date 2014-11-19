e2655e5bae7d41f4f3cdb3a95af5764d91b7e44b
burlistic <will.webster@mykolab.com>
2014-11-19 21:55:19 +0800

    Initial Commit

---
+++ AnagramAlgorithm/AnagramAlgorithm.Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+namespace AnagramAlgorithm.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+
+            Assert.Equal(1, 1);
+        }
+    }
+}
