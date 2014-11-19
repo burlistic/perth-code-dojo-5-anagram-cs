0e74a07faf14c56b41c72859179179b5091b8336
burlistic <will.webster@mykolab.com>
2014-11-19 22:18:02 +0800

    First test passing

---
+++ AnagramAlgorithm/AlgorithmEngine/AnagramEngine.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmEngine
+{
+    public class AnagramEngine
+    {
+        public static List<string> Find(string seedWord, List<string> wordList)
+        {
+
+            var matchedWords = new List<string>();
+            
+
+            
+
+            foreach (var word in wordList)
+            {
+
+                var match = true;
+
+                var seedChars = word.ToCharArray();
+
+                foreach (var seedChar in seedChars)
+                {
+
+                    if (!seedWord.Contains(seedChar))
+                    {
+                        match = false;
+                    }
+
+                }
+
+                if (match)
+                {
+                    matchedWords.Add(word);
+                }
+
+            }
+
+            return matchedWords;
+
+        }
+    }
+}
---
+++ AnagramAlgorithm/AnagramAlgorithm.Tests/AnagramEngineTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgorithmEngine;
+using Xunit;
+using Xunit.Sdk;
+
+namespace AnagramAlgorithm.Tests
+{
+
+    // FileReader ??
+
+    // Giant dictionay output
+
+    // Input - word, dictionay // Output, matches
+    
+    
+    public class AnagramEngineTests
+    {
+        [Fact]
+        public void FindASimpleAnagram()
+        {
+            List<string> matches = AnagramEngine.Find("cat", new List<string> {"at", " bla", "rah", "ct"}); // array?
+
+            Assert.Equal(2, matches.Count);
+        }
+    }
+
+    // test casing
+}
